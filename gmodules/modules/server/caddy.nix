e0a10ba9f94510421f01b9644d38b1fef395403d
Albert Gaitian <albert@gaitian.ru>
2026-04-15 20:49:34 +0300

    Wed 15 Apr 20:49:34 MSK 2026

--- gmodules/modules/server/caddy.nix
+++ gmodules/modules/server/caddy.nix
@@ -24,7 +24,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-        hash = "sha256-Zls+5kWd/JSQsmZC4SRQ/WS+pUcRolNaaI7UQoPzJA0=";
+        hash = "sha256-o0zYCp3h7E0L1cgmJwNtGNgzhmZk5BAVoWQA+eL+zqc=";
       };
       environmentFile = "/var/secrets/caddy.env";
       globalConfig = ''
